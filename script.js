4b69132e3cab6899328f788d9165eb163d1a1516
PK Banks <pkbanks12@gmail.com>
2017-12-21 20:07:53 -0500

    solved 'add message' test

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+$(function(){
 
+  $('#new-message-button').click(function(){
+    add_new_message();
+  });
+
+  
+});
+
+function add_new_message(){
+    // get message from #new-message-body
+    const message = $('#new-message-body').val();
+
+    // create a new li element in #conversation
+    // within it, add a span, and
+    // assign class message-body to the span
+    // add to that span the text from message
+    // insert the new_li into document, within #conversation
+    const content = `<li><span class="message-body">${message}</span></li>`;
+    $('#conversation').append(content);
+}
